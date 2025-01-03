270dbea4f5dcde8164a3f45d3fb77492c7dee25c
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-01-03 06:38:59 -0500

    working GET request

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const axios = require("axios");
 
 // URL of the API we're using
-const API_URL = "https://jsonplaceholder.typicode.com/posts";
-
+const API_KEY = "live_gecybbtg4DMF0IwPFn5AC9xqPKEhz02jvj2EcTPI8lXWit3A16xGC4CDI9V6EYA2";
+const API_URL = `https://api.thedogapi.com/v1/images/search?limit=10&api_key=${API_KEY}`;
 // Function to fetch posts (GET method)
 function fetchPosts() {
   return axios.get(API_URL)
@@ -17,6 +17,7 @@ function fetchPosts() {
   });
 }
 
+/*
 // Function to add a new post (POST method) using Async/Await
 async function addPost(postData) {
   try {
@@ -28,19 +29,25 @@ async function addPost(postData) {
     console.error("Error adding post:", error);
   }
 }
+ */
 
 // Function to demonstrate both GET and POST functionality
 async function runProject() {
   console.log("Fetching all posts...");
   await fetchPosts(); // Call the fetchPosts function (GET)
 
+  /*
   console.log("\nAdding a new post...");
   const newPost = {
     title: "My New Post",
     body: "This is the content of the new post.",
     userId: 1
+
   };
+
   await addPost(newPost); // Call the addPost function (POST)
+*/
+
 }
 
 // Run the project
